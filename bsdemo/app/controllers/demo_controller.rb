50bd4239af9e34e8d6059f099fad9d5a13036ca8
Pito Salas <pitosalas@gmail.com>
2015-03-19 21:22:27 -0400

    cleanup

--- bsdemo/app/controllers/demo_controller.rb
+++
@@ -1,7 +0,0 @@
-class DemoController < ApplicationController
-  def carousel
-  end
-
-  def simple
-  end
-end
--- bsdemo/test/controllers/demo_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class DemoControllerTest < ActionController::TestCase
-  test "should get carousel" do
-    get :carousel
-    assert_response :success
-  end
-
-end
